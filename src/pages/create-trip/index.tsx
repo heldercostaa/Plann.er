345a2ba95facdde8575dd5a7336b8f666110363b
Helder Costa <heldercostaaa@gmail.com>
2024-07-10 11:53:50 -0300

    refactor: refactor confirm-trip-modal into separated component

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, Calendar, Mail, MapPin, Plus, Settings2, User, UserRoundPlus, X } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, UserRoundPlus } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ConfirmTripModal } from "./confirm-trip-modal";
 import { InviteGuestsModal } from "./invite-guests-modal";
 
 export function CreateTripPage() {
@@ -60,7 +61,9 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList)
   }
 
-  function createTrip() {
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
     navigate('/trips/abc-123')
   }
 
@@ -140,50 +143,10 @@ export function CreateTripPage() {
       )}
 
       {isConfirmTripModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold">
-                  Confirm trip creation
-                </h2>
-                <button type="button" onClick={closeConfirmTripModal}>
-                  <X className="size-5 text-zinc-400" />
-                </button>
-              </div>
-              <p className="text-sm text-zinc-400">
-                To complete the trip creation to <span className="font-semibold text-zinc-100">Fortaleza, Brasil</span> between <span className="font-semibold text-zinc-100">16 and 27 of August 2024</span>, fill your data below:
-              </p>
-            </div>
-
-            <form onSubmit={closeConfirmTripModal} className="space-y-3">
-              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                <User className="text-zinc-400 size-5" />
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Your full name"
-                  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
-                />
-              </div>
-
-              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                <Mail className="text-zinc-400 size-5" />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Your personal email"
-                  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
-                />
-              </div>
-
-              <button onClick={createTrip} type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
-                Confirm trip creation
-                <Plus className="size-5" />
-              </button>
-            </form>
-          </div>
-        </div>
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
       )}
 
     </div>
