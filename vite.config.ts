1eada15e5eb642df5558d099890143e9bcf1c402
Helder Costa <heldercostaaa@gmail.com>
2024-07-15 02:03:53 -0300

    ci: test gh actions config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/[REPO_NAME]/",
+  base: "/[Plann.er]/",
   plugins: [react()],
   server: {
     port: 3000,
